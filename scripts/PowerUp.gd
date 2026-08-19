eaf85ed596984721efe3fb770d0862e3dcb4a3d4
Davi <cubonelindo@gmail.com>
2026-08-19 09:34:29 -0300

    colocando o texto

--- scripts/PowerUp.gd
+++ scripts/PowerUp.gd
@@ -1,9 +1,10 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+extends Area2D
+## Coletável da fase 
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		GameManager.add_point()
+		body.brilhar()
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/placa.gd
+++ scripts/placa.gd
@@ -6,11 +6,16 @@ extends Area2D
 var cerejas_na_fase = 5
 
 
-@onready var texto: Label = $Arbusto
-
+@onready var Mensagem: Label = $Arbusto
+@onready var label_mensagem: Label = $Mensagem
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
 
-	texto.text = "Fase concluida! Ao todo eram " + cerejas_na_fase + " cerejas."
+	Mensagem.text = "Fase concluida! Ao todo eram " + cerejas_na_fase + " cerejas."
+
+
+func _on_mensagem_focus_entered() -> void:
+	Mensagem 
+	label_mensagem.text = "Mensagem.text" +str(Mensagem)# Replace with function body.
